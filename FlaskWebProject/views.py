0567af416422d1c24c0a32c2283cea8b1c921d9e
vulpes-rex <kvwulp@gmail.com>
2022-10-03 09:52:43 -0400

    sixth commit

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -74,8 +74,10 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warning('Invalid login attempt.')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info(user.username + " has logged in.")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
